39404ab3d253f8c8b5a22ba7b12686eb88b22f1b
Salma Agrirane <agriranesalma99@gmail.com>
2026-09-15 12:16:25 +0100

    Add text area for user questions in app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
 import streamlit as st
 st.title("Welcome to Whisper Circle")
 st.set_page_config(page_title="Whisper Circle", page_icon="🎀", layout="centered")
+question = st.text_area("Ask Your Question Freely", height= 100)
