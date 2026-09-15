0d3e689dad01de1e97d8b8b7cbd10420891168cd
Salma Agrirane <agriranesalma99@gmail.com>
2026-09-15 12:14:04 +0100

    Set page configuration for Whisper Circle app

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 import streamlit as st
 st.title("Welcome to Whisper Circle")
-st.write("Testing again")
+st.set_page_config(page_title="Whisper Circle", page_icon="⋆˚✿˖° 𐙚 ₊ ⊹ ♡", layout="centered")
