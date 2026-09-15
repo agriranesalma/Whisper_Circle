a0f6200e569872710c0107b0e270ac288eb5ce7f
Salma Agrirane <agriranesalma99@gmail.com>
2026-09-15 15:54:40 +0100

    Update Streamlit page configuration and layout

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 st.title("Welcome to Whisper Circle")
-st.set_page_config(page_title="Whisper Circle", page_icon="🎀", layout="centered")
-question = st.text_area("Ask Your Question Freely", height= 100)
-
+st.set_page_config(
+page_title="Whisper",
+page_icon="",
+layout="wide", 
+initial_sidebar_state="collapsed"
+)
 with st.chat_message("user"):
   st.write("How long is a normal cycle?")
 with st.chat_message("assistant"):
