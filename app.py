cbccf53d1f74f18c04015586a6738a7cc5985691
Salma Agrirane <agriranesalma99@gmail.com>
2026-09-15 16:05:28 +0100

    Refactor Streamlit app configuration and chat input

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
 import streamlit as st
 st.title("Welcome to Whisper Circle")
-st.set_page_config(
-page_title="Whisper",
-page_icon="",
-layout="wide", 
-initial_sidebar_state="collapsed"
-)
-with st.chat_message("user"):
-  st.write("How long is a normal cycle?")
-with st.chat_message("assistant"):
-  st.write("Typically around 28 days, though it varies person to person.")
-prompt = st.chat_input("Ask something...")
+st.set_page_config(page_title="Whisper",page_icon="🎀",layout="wide", initial_sidebar_state="collapsed")
+
+
+prompt= st.chat_input("You are welcome to ask any question, this is a safe space")
 if prompt:
-  st.write(f"You asked: {prompt}")
+  st.write(f"User has sent the following prompt:{prompt}")
