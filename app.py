740ee19354587c548599599adee19eee2a5e51a8
Salma Agrirane <agriranesalma99@gmail.com>
2026-09-15 12:14:52 +0100

    Update page icon in Streamlit app configuration

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 import streamlit as st
 st.title("Welcome to Whisper Circle")
-st.set_page_config(page_title="Whisper Circle", page_icon="⋆˚✿˖° 𐙚 ₊ ⊹ ♡", layout="centered")
+st.set_page_config(page_title="Whisper Circle", page_icon="🎀", layout="centered")
