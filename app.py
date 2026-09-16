238ac5c0fc5c76c5db68cd2995afb569b96701d9
Salma Agrirane <agriranesalma99@gmail.com>
2026-09-16 01:11:27 +0100

    Add OpenAI integration and chat message handling
    Integrate OpenAI API for chat functionality and manage session state for messages.

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 import streamlit as st
+from openai import OpenAI
+
 st.title("Welcome to Whisper Circle")
 st.set_page_config(page_title="Whisper",page_icon="🎀",layout="wide", initial_sidebar_state="collapsed")
 
+client= OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+if "openai_model" not in st.session_state:
+  st.session_state["openai_model"]= "gpt-3.5-turbo"
 
-prompt= st.chat_input("You are welcome to ask any question, this is a safe space")
-if prompt:
-  st.write(f"User has sent the following prompt: {prompt}")
+if "messages" not in st.session_state:
+  st.session_state.messages=[]
+for message in st.session_state.messages:
+  with st.chat_message(message["role"]):
+    st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
